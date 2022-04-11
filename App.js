04582fce489ff5736b3b8898e0f865611b1c085f
Toxicann <anushkarki007@gmail.com>
2022-04-11 09:58:33 +0545

    notifics

--- App.js
+++ App.js
@@ -1,20 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { StyleSheet, Text, View, Button, Platform } from "react-native";
 
-export default function App() {
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldShowAlert: true,
+    shouldSetBadge: false,
+  }),
+});
+
+const App = () => {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+    // Notifications.addNotificationReceivedListener((notification) => {
+    //   alert(`we received a notification`);
+    // });
+    // console.log(expoPushToken);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Click Below to show notify current DateTime</Text>
+      <Button
+        title="Show DateTime!"
+        onPress={async () => {
+          await sendPushNotification(expoPushToken);
+          console.log(expoPushToken);
+        }}
+      />
     </View>
   );
-}
+};
+
+const sendPushNotification = async (expoPushToken) => {
+  const date = new Date().getDate();
+  const month = new Date().getMonth() + 1;
+  const year = new Date().getFullYear();
+  const hours = new Date().getHours();
+  const min = new Date().getMinutes();
+  const sec = new Date().getSeconds();
+
+  const message = {
+    to: expoPushToken,
+    sound: "default",
+    title: "Current DateTime",
+    body: `Date is: ${date}/${month}/${year} & Time is: ${hours}h: ${min}m: ${sec}s`,
+    android: {
+      channelId: "push-notifs",
+    },
+  };
+
+  await fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+};
+
+registerForPushNotificationsAsync = async () => {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+  } else {
+    alert("Must use physical device for Push Notifications");
+    return null;
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("push-notifs", {
+      name: "push-notifs",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#00000000",
+      sound: true,
+    });
+  }
+  return token;
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
